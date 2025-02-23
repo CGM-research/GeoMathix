e43bf1a73fa6f4bda76ae5c28001d3e1b88153a3
John Afanasieff <afanasieffivan@gmail.com>
2025-02-23 22:55:50 +0300

    Branch for experiments and learning OpenGL

--- main.cpp
+++ main.cpp
@@ -23,6 +23,10 @@ int32_t main() {
   glViewport(0, 0, 500, 500);
   
   while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.0f, 0.8f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
